25d715f6aaa9c8a2079510e1beaa1466046210d0
Abdallah Mohammed <abdallahabdulzahir@gmail.com>
2025-04-18 17:01:38 +0200

    Initial Commit

---
+++ Pokemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokemon
+//
+//  Created by name on 18/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by name on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
